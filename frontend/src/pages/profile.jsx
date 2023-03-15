091c62417239be3adac7d556ef3ea8d8d079e628
Aydrian Howard <aydrian@gmail.com>
2023-03-14 20:29:42 -0400

    profile page mocking

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
+const fakeUser = {
+  id: "b3a23e83-dd46-47ce-aefc-695bf8f347c3",
+  username: "craig",
+  full_name: "Craig Cockroach",
+  is_admin: true,
+  created_at: new Date(),
+  updated_at: new Date()
+};
+
 export default function ProfilePage() {
-  return <div>Profile</div>;
+  const [user, setUser] = React.useState(fakeUser);
+  return (
+    <>
+      <h2>Profile</h2>
+      <img src="https://picsum.photos/200" />
+      <h3>{user.username}</h3>
+      <h4>{user.full_name}</h4>
+    </>
+  );
 }
