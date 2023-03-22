34493ef0d233fa43381c7c97d5100922f0fe21ac
Aydrian Howard <aydrian@gmail.com>
2023-03-22 15:15:23 -0400

    fixed showing login errors

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -4,10 +4,10 @@ export const fetcher = (url) =>
   fetch(url, { credentials: "include" }).then(parseJson);
 
 export const parseJson = (response) => {
-  if (response.status === 401) {
-    throw new AuthorizationError();
-  }
   return response.json().then((json) => {
+    if (response.status === 401) {
+      throw new AuthorizationError(json.detail);
+    }
     if (!json.ok) {
       throw json;
     }
